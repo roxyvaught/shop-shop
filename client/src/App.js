41b55ad1e0561dd17833911d8ae2be62c1f96a51
roxyvaught <roxanna.vaught@gmail.com>
2020-08-22 18:41:46 -0500

    stripe function

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
+import Success from "./pages/Success";
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -38,6 +39,7 @@ function App() {
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
+              <Route exact path="/success" component={Success} />
             </Switch>
           </StoreProvider>
         </div>
--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,3 +1,4 @@
+import { useLazyQuery } from '@apollo/react-hooks';
 import React, { useEffect } from "react";
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
@@ -5,11 +6,15 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
 
 const [state, dispatch] = useStoreContext();
-console.log(state)
+const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
 useEffect(() => {
   async function getCart() {
@@ -22,6 +27,14 @@ useEffect(() => {
   }
 }, [state.cart.length, dispatch]);
 
+useEffect(() => {
+  if (data) {
+    stripePromise.then((res) => {
+      res.redirectToCheckout({ sessionId: data.checkout.session });
+    });
+  }
+}, [data]);
+
 function toggleCart() {
   dispatch({ type: TOGGLE_CART });
 }
@@ -32,6 +45,19 @@ function calculateTotal() {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+    getCheckout({
+      variables: { products: productIds }
+    });
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
   }
 
 if (!state.cartOpen) {
@@ -56,9 +82,9 @@ if (!state.cartOpen) {
         <strong>Total: ${calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
-              Checkout
-            </button>
+          <button onClick={submitCheckout}>
+          Checkout
+        </button>
             :
             <span>(log in to check out)</span>
         }
